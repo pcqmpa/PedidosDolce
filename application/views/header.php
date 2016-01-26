a3c64adf75fa1018a0807ff6fe2d75fcdcce4012
pcqmpa <matcomputo@hotmail.com>
2016-01-26 17:43:22 -0500

    Configuracion Inicial
    Archivos css

---
+++ application/views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/reticulas.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+</head>
+<body>
+<h2>Sistema de Digitacion de Pedidos Dolce</h2>
+</body>
+</html>
